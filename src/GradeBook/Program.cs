76c6ee04e33ebb3d8cd4da5fbca7fcf3c73ffbbf
Anna BURLEY <anna.burley@axa-ppp.co.uk>
2020-02-11 10:54:32 +0000

    initial commit

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+// using System.Collections.Generic;
+
+namespace GradeBook
+{
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var book = new Book("Scott's Grade Book");
+      book.AddGrade(89.1);
+      book.AddGrade(22.7);
+      book.AddGrade(40.5);
+      var stats = book.GetStatistics();
+      Console.WriteLine($"Lowest grade is {stats.Low}");
+      Console.WriteLine($"Highest grade is {stats.High}");
+      Console.WriteLine($"Average grade is {stats.Average:N1}");
+    }
+  }
+
+}
---
+++ test/Gradebook.Tests/BookTests.cs
@@ -0,0 +1,22 @@
+using System;
+using Xunit;
+
+namespace GradeBook.Tests
+{
+  public class BookTests
+  {
+    [Fact]
+    public void Test1()
+    {
+      var book = new Book("");
+      book.AddGrade(89.1);
+      book.AddGrade(60.2);
+      book.AddGrade(75.2);
+      var result = book.GetStatistics();
+
+      Assert.Equal(74.8, result.Average, 1);
+      Assert.Equal(89.1, result.High, 1);
+      Assert.Equal(60.2, result.Low, 1);
+    }
+  }
+}
