f2570025c3586f779fcad2202964d6f85f46f441
Anna BURLEY <anna.burley@axa-ppp.co.uk>
2020-02-27 16:53:17 +0000

    building  types

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -9,14 +9,43 @@ namespace GradeBook
     static void Main(string[] args)
     {
       var book = new Book("Scott's Grade Book");
-      book.AddGrade(89.1);
-      book.AddGrade(22.7);
-      book.AddGrade(40.5);
+      book.GradeAdded += OnGradeAdded;
+
+      while (true)
+      {
+        Console.WriteLine("Enter a grade or 'q' to exit");
+        var input = Console.ReadLine();
+        if (input == "q")
+        {
+          break;
+        }
+        try
+        {
+          var grade = double.Parse(input);
+          book.AddGrade(grade);
+        }
+        catch (Exception ex)
+        {
+          Console.WriteLine(ex.Message);
+        }
+        // catch (FormatException ex)
+        // {
+        //   Console.WriteLine(ex.Message);
+        // }
+        finally
+        {
+          Console.WriteLine("***");
+        }
+      }
       var stats = book.GetStatistics();
       Console.WriteLine($"Lowest grade is {stats.Low}");
       Console.WriteLine($"Highest grade is {stats.High}");
       Console.WriteLine($"Average grade is {stats.Average:N1}");
+      Console.WriteLine($"The letter grade is {stats.Letter}");
+    }
+    static void OnGradeAdded(object sender, EventArgs e)
+    {
+      Console.WriteLine("A grade was added");
     }
   }
-
 }
--- test/Gradebook.Tests/BookTests.cs
+++ test/Gradebook.Tests/BookTests.cs
@@ -14,9 +14,21 @@ namespace GradeBook.Tests
       book.AddGrade(75.2);
       var result = book.GetStatistics();
 
-      Assert.Equal(74.8, result.Average, 1);
+      Assert.Equal(74.8, result.Average, 1
+      );
       Assert.Equal(89.1, result.High, 1);
       Assert.Equal(60.2, result.Low, 1);
+      Assert.Equal('C', result.Letter);
     }
+    // [Fact]
+    // public void AddsGradeToBook()
+    // {
+    //   var book2 = new Book("My Book");
+    //   book2.AddGrade(101);
+    //   var result = book2.GetStatistics();
+    //   Assert.Equal(0, book2.grades.Count);
+    //   book2.AddGrade(99);
+    //   Assert.Equal(1, book2.grades.Count);
+    // }
   }
 }
